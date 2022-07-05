3a29f38295df643d83914681017b67efe533b84b
4869 <89888792+blairyblevy@users.noreply.github.com>
2022-07-05 23:02:44 +0900

    회계 프로그램
    상품가격을 통해 세금, 이익, 배당률, 소요비용을 계산하는 프로그램

---
+++ AccountingApp.java
@@ -0,0 +1,24 @@
+public class App1 {
+
+	public static void main(String[] args) {
+		
+		
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expensRate = 0.3;
+		double vat = valueOfSupply * vatRate;
+		double expense = valueOfSupply * expensRate;
+		double income = valueOfSupply - expense ;
+		double devidend1 = income * 0.5;
+		double devidend2 = income * 0.3;
+		double devidend3 = income * 0.2;
+		
+		
+		System.out.println("ValueOfSupply : "+ valueOfSupply);
+		System.out.println("Vat : " + vat);		
+		System.out.println("Expense : " + expense);
+		System.out.println("Income : " + income);
+		System.out.println("Devidend 1 = " + devidend1);
+		System.out.println("Devidend 2 = " + devidend2);
+		System.out.println("Devidend 3 = " + devidend3);
+}
